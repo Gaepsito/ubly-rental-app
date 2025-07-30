509fd583702f61faffdfc47e64823603d4338328
Gaepsito <paul.arellano@outlook.es>
2025-07-29 22:43:46 -0500

    Create HomePage.jsx

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Bienvenido a Ubly 🚙</h1>
+      <p>Explora autos, reserva tu viaje.</p>
+    </div>
+  );
+}
+
+export default HomePage;
