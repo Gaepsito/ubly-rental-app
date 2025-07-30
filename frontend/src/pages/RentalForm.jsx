c091b68519f388d33b4688ff7c1438aaeb8f51ec
Gaepsito <paul.arellano@outlook.es>
2025-07-29 22:45:11 -0500

    Create RentalForm.jsx

---
+++ frontend/src/pages/RentalForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RentalForm() {
+  return (
+    <div>
+      <h2>Formulario de Alquiler 📝</h2>
+      <p>Aquí irá el formulario para reservar un auto.</p>
+    </div>
+  );
+}
+
+export default RentalForm;
